61241e05d6e049edc3bcf6d6cb94220d540b0768
Eddie <87498041+edd-e98@users.noreply.github.com>
2022-12-16 17:23:57 -0800

    Add files via upload

---
+++ it261/website/about.php
@@ -0,0 +1,14 @@
+<?php
+    include('./includes/header.php');
+?>
+<div id="wrapper">
+    <div id="main">
+        <h1>Here are the screenshots of my databse for the "project" tab!</h1>
+        <img src="./images/adminer-upload.png" alt="adminer upload">
+        <img src="./images/adminer-login.png" alt="adminer login">
+        <img src="./images/adminer-table.png" alt="adminer table">
+    </div>
+</div>
+<?php
+    include('./includes/footer.php');
+?>
